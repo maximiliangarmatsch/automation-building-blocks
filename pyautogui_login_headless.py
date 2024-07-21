2b945e4c85274746b786eb59b755625f44b9da7c
USTADCOM <amirsohailali123@gmail.com>
2024-07-21 16:43:52 +0500

    update pyautogui_login_headless.py

--- pyautogui_login_headless.py
+++ pyautogui_login_headless.py
@@ -20,43 +20,68 @@ browser.save_screenshot( '1.png' )
 # mouse moves in SmartDisplay 
 pyautogui._pyautogui_x11._display = Xlib.display.Display(
      os.environ[ 'DISPLAY' ])
-try:
-    time.sleep(5)
-    pos = pyautogui.position()
-    print(pos)
-    time.sleep(4)
-    # Locate the image on the screen with a confidence threshold
-    cords_image = pyautogui.locateOnScreen('assets/bitwarden.png', confidence = 0.7)
-    if cords_image is not None:
-        cords_center = pyautogui.center(cords_image) # Find the center of detected icon
-        pyautogui.moveTo(cords_center[0], cords_center[1], 1) # Move mouse to specified position
-        pyautogui.click(cords_center[0], cords_center[1]) # Click on the located image
-        time.sleep(2) # Wait a bit to ensure the click action is registered
-        
-        cords_image_secret = pyautogui.locateOnScreen('assets/secret.png', confidence = 0.7)
-        cords_center_secret = pyautogui.center(cords_image_secret) # Find the center of detected icon
-        pyautogui.moveTo(cords_center_secret[0], cords_center_secret[1], 1) # Move mouse to specified position
-        pyautogui.click(cords_center_secret[0], cords_center_secret[1]) # Click on the located image
-        time.sleep(2) # Wait a bit to ensure the click action is registered
+def detect_icon(icon_path: str):
+    """
+    Method take icon image path as input and detect it on browser to get its coordinates.
 
-        cords_image_login = pyautogui.locateOnScreen('assets/login.png', confidence = 0.7)
-        cords_center_login = pyautogui.center(cords_image_login) # Find the center of detected icon
-        pyautogui.moveTo(cords_center_login[0], cords_center_login[1], 1) # Move mouse to specified position
-        pyautogui.click(cords_center_login[0], cords_center_login[1]) # Click on the located image
-        time.sleep(2) # Wait a bit to ensure the click action is registered
+    Parameters
+    ----------
+    icon_path: str
+        icon image path.
+
+    Return
+    ------
+    None
+    """
+    image_coordinates = pyautogui.locateOnScreen(icon_path, confidence = 0.7)
+    image_center_coordinates = pyautogui.center(image_coordinates)
+    pyautogui.moveTo(image_center_coordinates[0], image_center_coordinates[1], 1)
+    pyautogui.click(image_center_coordinates[0], image_center_coordinates[1])
+    return image_coordinates
+
+def login_via_bitwarden():
+    """
+    login_via_bitwarden method will to the specific url and step by step perfomr action to login to make.com via bitwarden.
+
+    Parameters
+    ----------
+    Nooe
+
+    Return
+    ------
+    None
+    """
+    try:
+        time.sleep(2)
+        pos = pyautogui.position()
+        print(pos)
+        time.sleep(2)
+
+        # Locate the bitwarden icon to click
+        cords_image = detect_icon("assets/bitwarden.png")
+        if cords_image is not None:
+            time.sleep(1)
+        
+        # Locate credentials in bitwarden
+        cords_image_secret = detect_icon("assets/secret.png")
+        if cords_image_secret is not None:
+            time.sleep(1)
+
+        # Locate the login button to click on it 
+        cords_center_login = detect_icon("assets/login.png")
+        if cords_center_login is not None:
+            time.sleep(1)
 
         # Take a screenshot after the action
         screenshot = pyautogui.screenshot()
         screenshot.save('action_screenshot.png')
 
-        # Print the coordinates of the image and center
-        print(cords_image, cords_center)
-except:
-    # Take a screenshot after the action
-    screenshot = pyautogui.screenshot()
-    screenshot.save('action_screenshot.png')
-    print("no magnifier found!")           
+    except:
+        # Take a screenshot after the action
+        screenshot = pyautogui.screenshot()
+        screenshot.save('action_screenshot.png')
+        print("No Icon image found!")           
 
-sleep(3)
+login_via_bitwarden()
 browser.quit ( )
 display.stop()
