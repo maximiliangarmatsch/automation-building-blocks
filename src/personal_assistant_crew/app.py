b69ed4ea0e16e10623acc60a0c569c28174871c8
USTADCOM <amirsohailali123@gmail.com>
2024-09-14 13:28:43 +0500

    app.py changes

--- src/personal_assistant_crew/app.py
+++ src/personal_assistant_crew/app.py
@@ -11,7 +11,7 @@ from langchain_openai import ChatOpenAI
 load_dotenv()
 
 # Initialize the language model
-llm = ChatOpenAI(model="gpt-4o")
+llm = ChatOpenAI(model="gpt-4o-mini")
 
 # Define tools for the agents
 composio_toolset = ComposioToolSet()
@@ -23,7 +23,7 @@ timezone = datetime.now().astimezone().tzinfo
 # Setup Todo
 invoices_netze_preparation_agent_todo = """
     Update data in the Google Sheet template {https://docs.google.com/spreadsheets/d/1HmDjB28iAil25HUByMQBTnKMGu0Vl5pzBhpqXWaFIrM/edit?gid=1194874101#gid=1194874101}.
-    For cell {I8}, set {number of upcoming month and year} and rember that 011 must be added wit month and year as {0924011}.
+    For cell {I8}, set {number of upcoming month and year} and rember that 011 must be added with month and year as {0924011}.
     For cell {I10}, set {first of the upcoming month} as {01.08.24}.
     For cell {I11}, set to {last of the upcoming month} as {30.09.24}.
     For cell {D16}, set {the month to the current month and year}  as {09.24}.
