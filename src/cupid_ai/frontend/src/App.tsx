9ad2e37c249f065d22f063ff885602c98150d4eb
Kayode <segunkayode00@gmail.com>
2024-09-28 04:15:37 -0700

    feat: top navigation (#33)
    * first commit
    
    * config: vscode settings
    
    * config: vscode settings
    
    * feat: top navigation

--- src/cupid_ai/frontend/src/App.tsx
+++ src/cupid_ai/frontend/src/App.tsx
@@ -1,27 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Container}  from "@mui/material";
+import { Container } from "@mui/material";
 import { PATHS } from "./utils";
 import { Profile } from "./pages/profile";
 import { Matches } from "./pages/matches";
 import { BottomNavigation } from "./components/BottomNavigation";
+import { TopNavigation } from "./components/TopNavigation";
 
 function App() {
-
-  const matchesData = [{
-    title: '24yo | Bartender | Berlin',
-    message: 'Chat-Date on Friday at 18:30, 29.09.2024'
-  }];
+  const matchesData = [
+    {
+      title: "24yo | Bartender | Berlin",
+      message: "Chat-Date on Friday at 18:30, 29.09.2024",
+    },
+  ];
 
   return (
-    <Container maxWidth="sm">
-      <BrowserRouter>
+    <BrowserRouter>
+      <TopNavigation />
+      <Container maxWidth="sm">
         <Routes>
           <Route path={PATHS.PROFILE} element={<Profile />} />
-          <Route path={PATHS.MATCHES} element={<Matches data={matchesData} />} />
+          <Route
+            path={PATHS.MATCHES}
+            element={<Matches data={matchesData} />}
+          />
         </Routes>
-        <BottomNavigation />
-      </BrowserRouter>
-    </Container>
+      </Container>
+      <BottomNavigation />
+    </BrowserRouter>
   );
 }
 
--- src/cupid_ai/frontend/src/components/BottomNavigation_FabButtons.tsx
+++ src/cupid_ai/frontend/src/components/BottomNavigation_FabButtons.tsx
@@ -1,23 +1,34 @@
-import React, {useState} from "react";
-import { Box, Fab, TextField, Typography, Paper, FormControlLabel, Checkbox, FormControl, RadioGroup, FormLabel, Radio} from "@mui/material";
-import FilterListIcon from '@mui/icons-material/FilterList';
-import Modal from '@mui/material/Modal';
-import Grid from '@mui/material/Grid2';
+import React, { useState } from "react";
+import {
+  Box,
+  Fab,
+  TextField,
+  Typography,
+  Paper,
+  FormControlLabel,
+  Checkbox,
+  FormControl,
+  RadioGroup,
+  FormLabel,
+  Radio,
+} from "@mui/material";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import Modal from "@mui/material/Modal";
+import Grid from "@mui/material/Grid2";
 
 const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
 export function BottomNavigation_FabButton() {
-
   const [open, setOpen] = useState(false);
 
   const onOpen = () => {
@@ -28,20 +39,23 @@ export function BottomNavigation_FabButton() {
     setOpen(false);
   };
 
-    return (
-      <>
-      <Fab color="primary" aria-label="add" 
+  return (
+    <>
+      <Fab
+        color="primary"
+        aria-label="add"
         onClick={onOpen}
         sx={{
-          position: 'absolute',
+          position: "absolute",
           zIndex: 9999,
-          top: -40,
+          transform: "translateY(-50%)",
           left: 0,
           right: 0,
-          margin: '0 auto',
-        }} >
-            <FilterListIcon />
-        </Fab>
+          margin: "0 auto",
+        }}
+      >
+        <FilterListIcon />
+      </Fab>
 
       <Modal
         open={open}
@@ -50,13 +64,18 @@ export function BottomNavigation_FabButton() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h1" sx={{ mb: 2 }}>
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h1"
+            sx={{ mb: 2 }}
+          >
             Filter your Matches
           </Typography>
 
           <Grid container spacing={2}>
             <Grid size={12}>
-              <Paper elevation={2} >
+              <Paper elevation={2}>
                 <Typography variant="h6">Gender</Typography>
                 <FormControl>
                   <RadioGroup
@@ -64,34 +83,51 @@ export function BottomNavigation_FabButton() {
                     aria-labelledby="demo-row-radio-buttons-group-label"
                     name="row-radio-buttons-group"
                   >
-                    <FormControlLabel value="female" control={<Radio />} label="Female" />
-                    <FormControlLabel value="male" control={<Radio />} label="Male" />
+                    <FormControlLabel
+                      value="female"
+                      control={<Radio />}
+                      label="Female"
+                    />
+                    <FormControlLabel
+                      value="male"
+                      control={<Radio />}
+                      label="Male"
+                    />
                   </RadioGroup>
                 </FormControl>
               </Paper>
             </Grid>
 
             <Grid size={12}>
-              <Paper elevation={2} >
+              <Paper elevation={2}>
                 <Typography variant="h6">Age</Typography>
                 <Box display="flex" flexDirection="row" gap={0}>
-                  <FormControlLabel control={<TextField size="small" sx={{ width: '60px' }} />} label=" To" />
-                  <FormControlLabel control={<TextField size="small" sx={{ width: '60px' }}  />} label="" />
+                  <FormControlLabel
+                    control={<TextField size="small" sx={{ width: "60px" }} />}
+                    label=" To"
+                  />
+                  <FormControlLabel
+                    control={<TextField size="small" sx={{ width: "60px" }} />}
+                    label=""
+                  />
                 </Box>
               </Paper>
             </Grid>
 
             <Grid size={12}>
-              <Paper elevation={2} >
+              <Paper elevation={2}>
                 <Typography variant="h6">Distance (km)</Typography>
                 <Box display="flex" flexDirection="row" gap={0}>
-                  <FormControlLabel control={<TextField size="small" sx={{ width: '80px' }} />} label="" />
+                  <FormControlLabel
+                    control={<TextField size="small" sx={{ width: "80px" }} />}
+                    label=""
+                  />
                 </Box>
               </Paper>
             </Grid>
           </Grid>
         </Box>
       </Modal>
-      </>
+    </>
   );
 }
