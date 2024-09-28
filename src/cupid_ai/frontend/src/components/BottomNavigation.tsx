12a6950c554298e690748eaaf8367eaa626adbec
Kayode <segunkayode00@gmail.com>
2024-09-28 04:15:37 -0700

    feat: top navigation (#33)
    * first commit
    
    * config: vscode settings
    
    * config: vscode settings
    
    * feat: top navigation

--- src/cupid_ai/frontend/src/components/BottomNavigation.tsx
+++ src/cupid_ai/frontend/src/components/BottomNavigation.tsx
@@ -1,55 +1,62 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Groups2Icon from "@mui/icons-material/Groups2";
 import PersonIcon from "@mui/icons-material/Person";
-import { Typography} from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { PATHS } from "../utils";
 import { BottomNavigation_FabButton } from "./BottomNavigation_FabButtons";
 
-export function BottomNavigation() {  
+export function BottomNavigation() {
   const [currentPath, setCurrentPath] = useState(
     () => window.location.pathname
   );
 
-    return (
+  return (
     <React.Fragment>
-      <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }}>
-        <Toolbar className="mx-3 flex justify-around">
-          <Link
-            to={PATHS.MATCHES}
-            onClick={() => setCurrentPath(PATHS.MATCHES)}
-          >
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              disabled={currentPath === PATHS.MATCHES}
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          position="fixed"
+          color="primary"
+          sx={{ top: "auto", bottom: 0 }}
+        >
+          <Toolbar className="mx-3 flex justify-around">
+            <Link
+              to={PATHS.MATCHES}
+              onClick={() => setCurrentPath(PATHS.MATCHES)}
             >
-              <Typography>
-                <Groups2Icon />
-                <br /> Matches
-              </Typography>
-            </IconButton>
-          </Link>
-          < BottomNavigation_FabButton />
-          <Link
-            to={PATHS.PROFILE}
-            onClick={() => setCurrentPath(PATHS.PROFILE)}
-          >
-            <IconButton
-              color="inherit"
-              disabled={currentPath === PATHS.PROFILE}
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                disabled={currentPath === PATHS.MATCHES}
+              >
+                <Typography>
+                  <Groups2Icon />
+                  <br /> Matches
+                </Typography>
+              </IconButton>
+            </Link>
+            <BottomNavigation_FabButton />
+            <Link
+              to={PATHS.PROFILE}
+              onClick={() => setCurrentPath(PATHS.PROFILE)}
             >
-              <Typography>
-                <PersonIcon />
-                <br />Me
-              </Typography>
-            </IconButton>
-          </Link>
-        </Toolbar>
-      </AppBar>
+              <IconButton
+                color="inherit"
+                disabled={currentPath === PATHS.PROFILE}
+              >
+                <Typography>
+                  <PersonIcon />
+                  <br />
+                  Me
+                </Typography>
+              </IconButton>
+            </Link>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </React.Fragment>
   );
 }
--- src/cupid_ai/frontend/src/pages/matches.tsx
+++ src/cupid_ai/frontend/src/pages/matches.tsx
@@ -1,21 +1,21 @@
-import {List, ListItemButton,ListItemText}  from "@mui/material";
+import { List, ListItemButton, ListItemText } from "@mui/material";
 import MatchesSort from "./matches_sort";
 
 export const Matches = (props: any) => {
-  const {data} = props;
+  const { data } = props;
   return (
     <>
       <MatchesSort />
       <List
-        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
         component="nav"
         aria-labelledby="nested-list-subheader"
       >
-        {data.map(match => 
+        {data.map((match) => (
           <ListItemButton key={match.title}>
             <ListItemText primary={match.title} secondary={match.message} />
           </ListItemButton>
-        )}
+        ))}
       </List>
     </>
   );
