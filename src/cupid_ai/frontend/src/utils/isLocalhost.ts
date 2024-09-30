abbaa06438ab01945aa6f1a8190054c0f5371cb6
zt4ff <segunkayode00@gmail.com>
2024-09-30 23:32:23 +0200

    fill: user image

--- src/cupid_ai/frontend/src/utils/isLocalhost.ts
+++ src/cupid_ai/frontend/src/utils/isLocalhost.ts
@@ -1,4 +1,2 @@
-// export const isLocalhost =
-//   window.location.hostname === "localhost" && window.location.port === "3000";
-
-export const isLocalhost = false;
+export const isLocalhost =
+  window.location.hostname === "localhost" && window.location.port === "3000";
